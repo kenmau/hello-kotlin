f198e98d0d9c4ffb922c6a2f02d2904f8cebe411
Ken Mau <>
2020-11-18 11:13:08 -0500

    Lesson 4.5

---
+++ HelloKotlin/src/main/kotlin/Spice/Spice.kt
@@ -0,0 +1,19 @@
+package Spice
+
+class Spice(val name: String, val spiciness: String = "mild") {
+    var heat: Int = 0
+        get() {
+            return when (spiciness) {
+                "mild" -> 5
+                "spicy" -> 7
+                "less than spicy" -> 3
+                else -> 0
+            }
+        }
+
+    init {
+        println("Spice Created: $name with heat $heat")
+    }
+}
+
+fun makeSalt() = Spice ("salt", "less than spicy")
--- HelloKotlin/src/main/kotlin/Spice/SpiceMain.kt
+++ HelloKotlin/src/main/kotlin/Spice/SpiceMain.kt
@@ -5,4 +5,24 @@ fun main() {
 
     println("Spice is named: ${spice.name}")
     println("Spice is level: ${spice.heat}")
+
+    val spiceList = listOf<Spice>(
+            Spice("a"),
+            Spice("b", "less than spicy"),
+            Spice("c", spiciness = "spicy"),
+            Spice("d", spiciness = "spicy")
+    )
+
+    val spicyList = spiceList.filter { it.heat > 5 }
+    print("Spicy List: ")
+
+    // These are 3 ways which a list can be iterated through:
+    //    (0 until someList.size).forEach { print(someList[it]) }
+    //    someList.forEach { print(it) }
+    //    someList.forEach(::print)
+
+    spicyList.forEach { print("${it.name} ") }
+    println()
+
+    val salt = makeSalt()
 }
