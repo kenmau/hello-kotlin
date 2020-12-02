fdf74bd7ae6bb9d75884520ea0822e6020d7e996
Ken Mau <>
2020-12-02 12:59:04 -0500

    Lesson 5.11

--- HelloKotlin/src/main/kotlin/Buildings/BaseBuildingMaterial.kt
+++ HelloKotlin/src/main/kotlin/Buildings/BaseBuildingMaterial.kt
@@ -1,4 +1,23 @@
 package Buildings
 
-class BaseBuildingMaterial {
+// Lesson 5.11
+open class BaseBuildingMaterial(open val numberNeeded: Int = 1) {
+
+}
+
+class Wood: BaseBuildingMaterial(4)
+
+class Brick: BaseBuildingMaterial(8)
+
+class Building<T: BaseBuildingMaterial>(val material: T) {
+    var baseMaterialsNeeded: Int = 100
+    var actualMaterialsNeeded: Int = material.numberNeeded * baseMaterialsNeeded
+
+    fun build() {
+        println("Type: ${material::class.simpleName}. Number Needed: ${actualMaterialsNeeded}")
+    }
+}
+
+fun main(args: Array<String>) {
+    val building = Building<Wood>(Wood()).build()
 }
